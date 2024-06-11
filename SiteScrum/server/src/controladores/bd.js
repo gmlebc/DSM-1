947265367b0c1614e5ce136222d9b1a6faabaae1
gmlebc <gmlebc@gmail.com>
2024-06-11 17:18:38 -0300

    somente arquivos necessarios

--- SiteScrum/server/src/controladores/bd.js
+++ SiteScrum/server/src/controladores/bd.js
@@ -5,24 +5,24 @@ dotenv.config();
 
 // Configura o pool de conexão, passando um objeto de configuração 
 // contendo as informações necessárias para se conectar ao BD do PostgreSQL
-const pool = new Pool({
-  user: process.env.BDUSUARIO,
-  host: process.env.BDHOST,
-  database: process.env.BDNOME,
-  password: process.env.BDSENHA,
-  port: process.env.BDPORTA
-});
+// const pool = new Pool({
+//   user: process.env.BDUSUARIO,
+//   host: process.env.BDHOST,
+//   database: process.env.BDNOME,
+//   password: process.env.BDSENHA,
+//   port: process.env.BDPORTA
+// });
 
 
 // Configura o pool de conexão, passando um objeto de configuração 
 // A URI de conexão precisa estar na variável de ambiente BDURI do arquivo .env
-/*const pool = new Pool({
-  connectionString: process.env.BDURI,
-  ssl: {
-    rejectUnauthorized: false // Ajuste necessário caso esteja utilizando SSL e seu ambiente requeira essa configuração
-  }
+const pool = new Pool({
+connectionString: process.env.BDURI,
+ssl: {
+  rejectUnauthorized: false // Ajuste necessário caso esteja utilizando SSL e seu ambiente requeira essa configuração
+}
 });
-*/
+
 // Exporta a variável pool
 module.exports = { pool };
 
